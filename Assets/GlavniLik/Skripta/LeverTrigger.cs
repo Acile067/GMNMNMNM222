09d0ce6ab6a99b069ca51f813a232394f6189327
LogerzGit <72897578+LogerzGit@users.noreply.github.com>
2021-01-29 16:06:17 +0100

    added sound effects and lever

---
+++ Assets/GlavniLik/Skripta/LeverTrigger.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LeverTrigger : MonoBehaviour
+{
+    [SerializeField] private Animator myAnimationController;
+
+    public AudioSource rattleSource;
+
+    private void Start() {
+        rattleSource = GetComponent<AudioSource>();
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            rattleSource.Play();
+            myAnimationController.SetBool("isActive", true);
+        }
+
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            myAnimationController.SetBool("isActive", false);
+        }
+
+    }
+}
